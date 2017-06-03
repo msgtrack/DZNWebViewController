2652cc759a99d9862fd973b94838534f280d1b7d
msgtrack <msgtrack>
2017-06-02 20:53:54 -0700

    Add nil checks

--- Source/Classes/DZNWebViewController.m
+++ Source/Classes/DZNWebViewController.m
@@ -422,7 +422,12 @@ static char DZNWebViewControllerKVOContext = 0;
 
 - (void)setTitle:(NSString *)title
 {
-    if (self.webNavigationPrompt == DZNWebNavigationPromptNone) {
+	if( title == nil )
+	{
+		return;
+	}
+	
+	if (self.webNavigationPrompt == DZNWebNavigationPromptNone) {
         [super setTitle:title];
         return;
     }
@@ -538,7 +543,12 @@ static char DZNWebViewControllerKVOContext = 0;
 
 - (void)loadURL:(NSURL *)URL
 {
-    NSURL *baseURL = [[NSURL alloc] initFileURLWithPath:URL.path.stringByDeletingLastPathComponent isDirectory:YES];
+	if( URL == nil )
+	{
+		return;
+	}
+	
+	NSURL *baseURL = [[NSURL alloc] initFileURLWithPath:URL.path.stringByDeletingLastPathComponent isDirectory:YES];
     [self loadURL:URL baseURL:baseURL];
 }
 
